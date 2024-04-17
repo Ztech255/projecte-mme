eed5fd89c637733fe3838ed8bbaed44407847c1f
Ztech255 <lluctaltavull@gmail.com>
2024-04-17 19:20:43 +0200

    Iconos

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./LoginForm.css";
+import { FaUser, FaLock } from "react-icons/fa";
 
 const LoginForm = () => {
   return (
@@ -8,9 +9,11 @@ const LoginForm = () => {
         <h1>Login</h1>
         <div className="input-box">
           <input type="text" placeholder="Username" Required />
+          <FaUser />
         </div>
         <div className="input-box">
           <input type="password" placeholder="password" Required />
+          <FaLock />
         </div>
         <div ClassName="remeber-forgot">
           <label>
