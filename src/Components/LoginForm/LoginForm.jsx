aa33de6576eb622e41ed07868a84778f1d1ebb4d
Ztech255 <lluctaltavull@gmail.com>
2024-04-22 18:54:32 +0200

    login css

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -20,11 +20,9 @@ const LoginForm = () => {
           <a href="#">Forgot Password</a>
         </div>
         <button type="submit">Login</button>
-        <div classname="register-link">
-          <p>
-            Don't have an account? <a href="#">Register</a>
-          </p>
-        </div>
+        <div className='register-link'>
+                    <p>Don't have an account? <a href='#'>Register</a></p>
+                </div>
       </form>
     </div>
   );
