60b35c51a5cd51f7e75593efdf86792980e4a0d5
Ztech255 <lluctaltavull@gmail.com>
2024-04-22 19:06:31 +0200

    progress

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,28 +1,48 @@
 import React from "react";
 import "./LoginForm.css";
 import { FaUser, FaLock } from "react-icons/fa";
+import { CgPassword } from "react-icons/cg";
 
 const LoginForm = () => {
+  var [setUser,user] = useState()
+    var [setPassword, password] = useState();
+  
+  var userDb = [
+    {
+      user: "user",
+      password: "1234",
+    },
+    {
+      user: "Lluc",
+      password: "1234",
+    },
+  ];
+
   return (
     <div className="wrapper">
       <form action="">
         <h1>Login</h1>
         <div className="input-box">
           <input type="text" placeholder="Username" required />
-          <FaUser className="icon"/>
+          <FaUser className="icon" />
         </div>
         <div className="input-box">
           <input type="password" placeholder="password" required />
-          <FaLock className="icon"/>  
+          <FaLock className="icon" />
         </div>
         <div className="remember-forgot">
-          <label><input type="checkbox" />remember me</label>
+          <label>
+            <input type="checkbox" />
+            remember me
+          </label>
           <a href="#">Forgot Password</a>
         </div>
         <button type="submit">Login</button>
-        <div className='register-link'>
-                    <p>Don't have an account? <a href='#'>Register</a></p>
-                </div>
+        <div className="register-link">
+          <p>
+            Don't have an account? <a href="#">Register</a>
+          </p>
+        </div>
       </form>
     </div>
   );
